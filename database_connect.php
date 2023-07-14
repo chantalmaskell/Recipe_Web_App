5faaf820a354551a3de1df00d636e32846e0d6df
Carson, Rob <r.carson@liverpool.ac.uk>
2023-07-14 13:27:19 +0100

    Comments added

--- database_connect.php
+++ database_connect.php
@@ -1,12 +1,15 @@
 <?php
 
+//MySQL database connection details
 $host = "localhost";
 $database_name = "recipe_login";
 $db_username = "root";
 $db_password = "";
 
+//Creates an object for the MySQL connection
 $sql_object = new mysqli(hostname: $host, database: $database_name, username: $db_username, password: $db_password);
 
+//Checks for a database connection error. Throws an error if it can't connect
 if ($sql_object->connect_errno) {
     die("Error when connecting: " . $sql_object->connect_error);
 }
