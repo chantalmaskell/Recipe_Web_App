4aeba611c5808f0e82d3f71f867a4094cd161f46
Carson, Rob <r.carson@liverpool.ac.uk>
2023-07-14 14:41:36 +0100

    Added login page
    Created a page for a user to login using email and password

---
+++ login_page.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+</head>
+<body>
+
+    <h1>Login</h1>
+
+    <form method="post">
+
+        <!--Area to input email address-->
+        <div>
+            <label for="email">Email address</label>
+            <input type="email" class="email" name="email">
+        </div>
+
+        <!--Area to input password-->
+        <div>
+            <label for="password">Password</label>
+            <input type="password" class="password" name="password">
+        </div>
+
+        <!--Login button-->
+        <button>Log in</button>
+    
+    </form>
+
+    
+</body>
+</html>
