2af07c0c083b189b4fa418267e9995f59319e520
Islam Mostafa <islam.maim@gmail.com>
2023-07-22 14:48:42 +0100

    Fixing remove recipe bug once click without refreshing the page

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 // JS file to handle the save and remove buttons while calling the backend and storing the necessary information.
 
 // Function to handle the "Save" or "Remove" button click
-function handleSaveButtonClick(recipeId, userId, isSaved) {
+async function handleSaveButtonClick(recipeId, userId, isSaved) {
     // Check if the user is logged in
     if (isLoggedIn) {
         var action = isSaved ? "remove_recipe" : "save_recipe";
         var buttonText = isSaved ? "Save" : "Remove";
 
-        // Send a request to the server to save or remove the recipe
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                alert(this.responseText);
-                // If successful, update the button text and toggle isSaved value
-                var button = document.querySelector(".save-button[data-recipe-id='" + recipeId + "']");
-                button.textContent = buttonText;
-                button.classList.toggle("save-button");
-                button.classList.toggle("remove-button");
-            }
-        };
+        try {
+            // Send a request to the server to save or remove the recipe
+            const response = await fetch(`index.php?action=${action}&recipe_id=${recipeId}`);
+            const data = await response.text();
 
-        xhttp.open("GET", "index.php?action=" + action + "&recipe_id=" + recipeId, true);
-        xhttp.send();
+            // Handle the response
+            if (response.ok) {
+                alert(data);
+
+                // If successful, update the button text and toggle isSaved value
+                var button = document.querySelector(`.save-button[data-recipe-id='${recipeId}']`);
+                if (!button) {
+                    button = document.querySelector(`.remove-button[data-recipe-id='${recipeId}']`);
+                }
+
+                if (button) {
+                    button.textContent = buttonText;
+                    button.classList.toggle("save-button");
+                    button.classList.toggle("remove-button");
+                } else {
+                    throw new Error(`Button not found for recipeId ${recipeId}`);
+                }
+            } else {
+                throw new Error(data);
+            }
+        } catch (error) {
+            alert(`An error occurred: ${error.message}`);
+        }
     } else {
         // If the user is not logged in, show a login message
         alert("Please log in to save or remove this recipe.");
