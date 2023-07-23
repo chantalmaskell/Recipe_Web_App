0947826e204cd67dc76963089a3487319432efa3
Seng, Wai I <w.seng@liverpool.ac.uk>
2023-07-23 09:53:28 +0000

    use information in database to search

--- script.js
+++ script.js
@@ -1,90 +1,93 @@
-//Recipe Date
-const recipes = [
-    {
-      name: 'Spaghetti Bolognese',
-      description: 'Once you \’ve got this grown-up spag bol going the hob will do the rest. Any leftovers will taste even better the next day.',
-      ingredients: ['2 tbsp olive oil or sun-dried tomato oil from the jar', '6 rashers of smoked streaky bacon, chopped', '2 large onions, chopped', 'onion', 'garlic', 'olive oil', 'salt', 'pepper'],
-      instructions: 'Boil the spaghetti. In a separate pan, saute the onion and garlic in olive oil. Add the ground beef and cook until browned. Add the tomato sauce, salt, and pepper, and simmer for 15 minutes. Serve the sauce over the spaghetti.',
-      categories: ['pasta', 'meat', 'main']
-      
-    },
-    {
-      name: 'Chicken Alfredo',
-      description: 'A creamy pasta dish with grilled chicken.',
-      ingredients: ['fettuccine','chicken breast', 'heavy cream', 'butter', 'garlic', 'Parmesan cheese', 'salt', 'pepper'],
-      instructions: 'Grill the chicken and cut it into strips. Boil the fettuccine. In a separate pan, melt the butter and saute the garlic. Add the heavy cream and simmer for a few minutes. Add the Parmesan cheese, salt, and pepper, and stir until the sauce is creamy. Serve the sauce and chicken over the fettuccine.',
-      categories:['main', 'meat']
-    }
-    // add more recipes here
-  ];
-  
 // Populate category select options
 const categorySelect = document.getElementById('categorySelect');
-const uniqueCategories = [...new Set(recipes.flatMap(recipe => recipe.categories))];
-uniqueCategories.forEach(category => {
-  const option = document.createElement('option');
-  option.value = category;
-  option.text = category;
-  categorySelect.appendChild(option);
-});
+fetch('/getCategories') // new endpoint which returns all categories
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(category => {
+      const option = document.createElement('option');
+      option.value = category;
+      option.text = category;
+      categorySelect.appendChild(option);
+    });
+  });
 
 document.getElementById('recipeForm').addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent the form from submitting normally
+  event.preventDefault();
   const searchType = document.getElementById('searchType').value;
   const keyword = (searchType !== 'category') ? document.getElementById('searchInput').value.toLowerCase() : document.getElementById('categorySelect').value;
   searchRecipes(keyword, searchType);
 });
 
-function searchRecipes(keyword, type) { // This function searches the recipes based on the keyword and type.
+async function searchRecipes(keyword, type) {
   let results;
-  switch (type) {
-    case 'name':
-        //If type is 'name', filter the recipes that include the keyword in the name.
-        results = recipes.filter(recipe => recipe.name.toLowerCase().includes(keyword));
-        break;
-    case 'ingredient':
-        // If type is 'ingredient', filter the recipes that include the keyword in the ingredients.
-        results = recipes.filter(recipe => recipe.ingredients.some(ingredient => ingredient.toLowerCase().includes(keyword)));
-        break;
-    case 'category':
-         // If type is 'category', filter the recipes that include the keyword in the categories.
-        results = recipes.filter(recipe => recipe.categories.includes(keyword));
-        break;
-    default:
-        // If type is not 'name', 'ingredient', or 'category', return an empty array.
-        results = [];
+  try {
+    const response = await fetch(`/search?${type}=${keyword}`);
+    results = await response.json();
+  } catch (error) {
+    console.error('An error occurred:', error);
+    return;
   }
 
-// sorting results by name in alphabetical order
-results.sort((a, b) => a.name.localeCompare(b.name));
-
-// Grabbing the recipeContainer element from the DOM.
-const recipeContainer = document.getElementById('recipeContainer'); 
-recipeContainer.innerHTML = ''; // Clear previous search results
-  
-// Checking if there are results and displaying them, otherwise show 'No recipes found'.
-  if (results.length > 0) {
-    results.forEach(recipe => {
-      const recipeElement = document.createElement('div'); // Creating new div for each recipe.
-      // Adding the details of the recipe to the div.
-      recipeElement.innerHTML = `
-        <h2>${recipe.name}</h2>
-        <h3>Categories</h3>
-        <p>${recipe.categories.join(', ')}</p>
-        <h3>Description</h3>
-        <p>${recipe.description}</p>
-        <h3>Ingredients</h3>
-        <p>${recipe.ingredients.join(', ')}</p>
-        <h3>Instructions</h3>
-        <p>${recipe.instructions}</p>
-        <hr>
-      `;
-      recipeContainer.appendChild(recipeElement); // Adding the div to the recipeContainer.
-    });
-  } else {
-    recipeContainer.innerText = 'No recipes found.';
-  }
+  displayResults(results);
 }
+
+function displayResults(results) {
+    const recipeContainer = document.getElementById('recipeContainer'); 
+    recipeContainer.innerHTML = ''; // Clear previous search results
+  
+    // Checking if there are results and displaying them, otherwise show 'No recipes found'.
+    if (results.length > 0) {
+      // Sorting results by recipe ID then by step number.
+      results.sort((a, b) => a.recipe_id - b.recipe_id || a.Step_number - b.Step_number);
+  
+      // Grouping results by recipe ID.
+      let groupedResults = results.reduce((acc, curr) => {
+        if(!acc[curr.recipe_id]) {
+          acc[curr.recipe_id] = {
+            Name: curr.Name,
+            Description: curr.Description,
+            Prep_time: curr.Prep_time,
+            Cook_time: curr.Cook_time,
+            Rating: curr.Rating,
+            Ingredients: curr.Ingredients,
+            Categories: curr.Categories,
+            Steps: []
+          };
+        }
+        acc[curr.recipe_id].Steps.push(`${curr.Step_number}. ${curr.Step_Description}`);
+        return acc;
+      }, {});
+  
+      // Converting the object back to an array of recipes and sorting it by recipe name.
+      let finalResults = Object.values(groupedResults).sort((a, b) => a.Name.localeCompare(b.Name));
+  
+      finalResults.forEach(recipe => {
+        const recipeElement = document.createElement('div'); // Creating new div for each recipe.
+        // Adding the details of the recipe to the div.
+        recipeElement.innerHTML = `
+          <h2>${recipe.Name}</h2>
+          <h3>Categories</h3>
+          <p>${recipe.Categories}</p>
+          <h3>Description</h3>
+          <p>${recipe.Description}</p>
+          <h3>Preparation Time</h3>
+          <p>${recipe.Prep_time}</p>
+          <h3>Cooking Time</h3>
+          <p>${recipe.Cook_time}</p>
+          <h3>Rating</h3>
+          <p>${recipe.Rating}</p>
+          <h3>Ingredients</h3>
+          <p>${recipe.Ingredients.split(',').join('<br/>')}</p>
+          <h3>Steps</h3>
+          <p>${recipe.Steps.join('<br/>')}</p>
+          <hr>
+         `;
+        recipeContainer.appendChild(recipeElement); // Adding the div to the recipeContainer.
+      });
+    } else {
+      recipeContainer.innerText = 'No recipes found.';
+    }
+  }  
 
 function updateSearchInput(value) {  // This function is used to toggle the visibility of the search input and category select based on the search type selected by the user.
     const searchInput = document.getElementById('searchInput');
